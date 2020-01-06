771e6ae9bb4c15ae86f61958f33257bef409c022
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-01-06 12:31:49 +0100

    Init

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+exports.CONTENT_PER_PAGE = 8;
+exports.PAGE_PER_SCREEN = 4;
+
+exports.POST = 'post';
+exports.PORTFOLIO = 'portfolio';
+exports.RESUME = 'resume';
+
+// Site Information
+exports.NAME = 'Web Design Journal';
+exports.TITLE = 'Web Design Journal';
+exports.PREFIX = 'LUCYBO | ';
+exports.DESCRIPTION = 'Web Design Journal';
+exports.AUTHOR = 'Lucyna Borkowska';
+exports.EMAIL = 'lucyna.indulska@gmail.com';
+exports.KEYWORDS = [
+  'Gatsby',
+  'ReactJS',
+].join(', ');
+exports.SITE_URL = 'https://lucybo.github.io';
+exports.PROFILE = 'https://avatars3.githubusercontent.com/u/42737121?s=200&v=4';
+
+// Social
+exports.GITHUB_ID = 'LucyBo';
+exports.TWITTER_ID = 'LucyBorkowska';
+exports.FACEBOOK_ID = 'lucyna.borkowska';
+exports.LINKEDIN_ID = 'lucyna-borkowska-9027a6184';
+exports.DISQUS_ID = 'LucynaBo';
